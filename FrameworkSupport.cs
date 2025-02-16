ab5045dc0be995563ef3c7f46554dece6457e7a2
冯 <2789083329@qq.com>
2025-02-16 18:12:59 +0800

    优化framework下的hash计算

--- FrameworkSupport.cs
+++ FrameworkSupport.cs
@@ -8,27 +8,8 @@ namespace FastHotKeyForWPF
         {
             unchecked
             {
-                return (int)((h1 << 5) + h1) ^ (int)h2;
-            }
-        }
-
-        public static int HashCombine(uint h1, uint h2, uint h3)
-        {
-            unchecked
-            {
-                int hash = HashCombine(h1, h2);
-                hash = (hash << 5) + hash ^ (int)h3;
-                return hash;
-            }
-        }
-
-        public static int HashCombine(uint h1, uint h2, uint h3, uint h4)
-        {
-            unchecked
-            {
-                int hash = HashCombine(h1, h2, h3);
-                hash = (hash << 5) + hash ^ (int)h4;
-                return hash;
+                uint rol5 = (h1 << 5) | (h1 >> 27);
+                return ((int)rol5 + (int)h1) ^ (int)h2;
             }
         }
     }
